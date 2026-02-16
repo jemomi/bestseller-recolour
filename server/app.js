ec4f5130ba7b4fbdf89e954a4cf44d9268ff6e75
Jens Morten Mikkelsen <dkjemomi@gmail.com>
2026-02-16 22:02:15 +0100

    Updated to express 5. And changed var to const

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
